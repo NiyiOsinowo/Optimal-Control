4c0b2459559aa55bba534ef16011de203e4a6574
Niyi1 <77877466+NiyiOsinowo@users.noreply.github.com>
2024-07-29 16:04:31 +0100

    Jul 29

--- Tools/ParticlesandFields.py
+++ Tools/ParticlesandFields.py
@@ -6,19 +6,37 @@ from abc import ABC, abstractmethod
 from EnforceTyping import EnforceClassTyping
 
 @dataclass
-class Particle(EnforceClassTyping):
+class Field(ABC):
+  Dimensionality: tuple # Represents the dimensionality of the vector space
+  @abstractmethod
+  def Dynamics(self, ObservationPosition: np.ndarray)-> np.ndarray:
+    # A function that returns the field vector at a given position and/or time 
+    ...
+  
+@dataclass
+class ClassicalParticle(EnforceClassTyping):
     'This class represents the electric field sources with its position in the field(Position) and the magnitude of the source(Charge)'
     Mass: float # kg
     Charge: float #C
 
 @dataclass
-class Field(ABC):
+class ClassicalField(Field):
   Dimensionality: tuple
   @abstractmethod
-  def Dynamics(self, ObservationPosition: np.ndarray)-> np.ndarray:
+  def Dynamics(self, ObservationPosition: np.ndarray, Time: float)-> np.ndarray:
     ...
-  @abstractmethod
-  def FieldPotential(self, ObservationPosition: np.ndarray)-> float:
+  def Potential(self, ObservationPosition: np.ndarray, Time: float)-> float:
+    # A function that returns the potential at a given position and/or time in the vector field  
     ...
-  def PotentialDifference(self, InitialPosition: np.ndarray, FinalPosition: np.ndarray) -> float:
+  def PotentialDifference(self, InitialPosition: np.ndarray, FinalPosition: np.ndarray, Time: float)-> float:
+    # A function that returns the potential difference between two positions at a given time in the vector field  
+    ...
+  def Gradient(self, ObservationPosition: np.ndarray, Time: float)-> float:
+    # A function that returns the gradient at a given position and/or time in the vector field
+    ...
+  def Curl(self, ObservationPosition: np.ndarray, Time: float)-> float:
+    # A function that returns the curl at a given position and/or time in the vector field
+    ...
+  def Divergence(self, ObservationPosition: np.ndarray, Time: float)-> float:
+    # A function that returns the divergence at a given position and/or time in the vector field
     ...
