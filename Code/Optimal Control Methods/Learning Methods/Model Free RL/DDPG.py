bb3bf62b614f0e221682a39c1afb5d853aa44284
Niyi1 <77877466+NiyiOsinowo@users.noreply.github.com>
2024-08-15 22:21:45 +0100

    Aug 15

--- Code/Optimal Control Methods/Learning Methods/Model Free RL/DDPG.py
+++ Code/Optimal Control Methods/Learning Methods/Model Free RL/DDPG.py
@@ -30,40 +30,46 @@ class DDPGAgent(LearningAgent):
                  actor_learning_rate: float,
                  critic_learning_rate: float,
                  soft_update_rate: float,
-                 ControlInterval: float = 0.5,
+                 control_interval: float = 0.5,
+                 control_magnitude: float= 1e-7,
                  discount_rate: float =0.99,
                  max_size: int= 1000,
                  batch_size: int= 6):
         self.environment= environment
-        self.policy: ActorNetwork = ActorNetwork(observation_size, action_size, actor_layers, actor_activations, 'DDPGMainActor', actor_learning_rate)
-        self.value_estimator = CriticNetwork(observation_size, action_size, critic_layers, critic_activations, 'DDPGMainCritic', critic_learning_rate)
-        self.target_policy = ActorNetwork(observation_size, action_size, actor_layers, actor_activations, 'DDPGTargetActor', actor_learning_rate)
-        self.target_value_estimator = CriticNetwork(observation_size, action_size, critic_layers, critic_activations, 'DDPGTargetCritic', critic_learning_rate)
-        for target_param, param in zip(self.target_policy.parameters(), self.policy.parameters()):
+        self.actor = ActorNetwork(observation_size, action_size, actor_layers, actor_activations, 'DDPGMainActor', actor_learning_rate)
+        self.critic = CriticNetwork(observation_size, action_size, critic_layers, critic_activations, 'DDPGMainCritic', critic_learning_rate)
+        self.target_actor = ActorNetwork(observation_size, action_size, actor_layers, actor_activations, 'DDPGTargetActor', actor_learning_rate)
+        self.target_critic = CriticNetwork(observation_size, action_size, critic_layers, critic_activations, 'DDPGTargetCritic', critic_learning_rate)
+        for target_param, param in zip(self.target_actor.parameters(), self.actor.parameters()):
             target_param.data.copy_(param.data)
-        for target_param, param in zip(self.target_value_estimator.parameters(), self.value_estimator.parameters()):
+        for target_param, param in zip(self.target_critic.parameters(), self.critic.parameters()):
             target_param.data.copy_(param.data) 
         self.memory = deque(maxlen=max_size)
         self.soft_update_rate= soft_update_rate
-        self.ControlInterval= ControlInterval
+        self.control_interval= control_interval
         self.discount_rate= discount_rate
         self.batch_size= batch_size
         self.noise = OUNoise(mu=np.zeros(action_size))
-        self.ControlInterval= self.ControlInterval# Action duration= conrol interval
+        self.control_interval= self.control_interval# Action duration= conrol interval
+        self.control_magnitude= control_magnitude
         self.update_network_parameters()
 
-    def observe(self, state= None):
+    @property
+    def policy(self):
+        return self.actor
+
+    def observe(self, state= None)-> T.Tensor:
         if state is None:
           state= self.environment.current_state   
         observation= T.Tensor(state.vector())
         return observation
   
     def act(self, observation: T.Tensor, with_noise: bool= True):
-        self.policy.eval()
-        observation = observation.to(self.policy.device)
-        action = self.policy.forward(observation).to(self.policy.device)
+        self.actor.eval()
+        observation = observation.to(self.actor.device)
+        action = self.policy(observation).to(self.actor.device)
         noise= T.tensor(self.noise(), dtype=T.float)
-        noisy_action = (1e-7* (action + noise)).to(self.policy.device)
+        noisy_action = (self.control_magnitude* (action + noise)).to(self.actor.device)
         if with_noise:
             return noisy_action.cpu().detach()
         else:
@@ -76,38 +82,38 @@ class DDPGAgent(LearningAgent):
         batch = random.sample(self.memory, self.batch_size)
         observations, actions, next_observations, rewards, dones = zip(*batch)
 
-        state = T.stack(observations).to(self.value_estimator.device)
-        action = T.stack(actions).to(self.value_estimator.device)
-        reward = T.tensor(rewards, dtype=T.float).unsqueeze(1).to(self.value_estimator.device)
-        new_state = T.stack(next_observations).to(self.value_estimator.device)
-        done = T.tensor(dones, dtype=T.float).unsqueeze(1).to(self.value_estimator.device)
+        state = T.stack(observations).to(self.critic.device)
+        action = T.stack(actions).to(self.critic.device)
+        reward = T.tensor(rewards, dtype=T.float).unsqueeze(1).to(self.critic.device)
+        new_state = T.stack(next_observations).to(self.critic.device)
+        done = T.tensor(dones, dtype=T.float).unsqueeze(1).to(self.critic.device)
         
-        self.target_policy.eval()
-        self.target_value_estimator.eval()
-        self.value_estimator.eval()
+        self.target_actor.eval()
+        self.target_critic.eval()
+        self.critic.eval()
         
-        target_actions = self.target_policy.forward(new_state)
-        Critic_value_ = self.target_value_estimator.forward(new_state, target_actions) 
-        q_expected = self.value_estimator.forward(state, action)
-        q_targets = reward + self.discount_rate * Critic_value_ * (1 - done)
+        target_actions = self.actor.forward(new_state)
+        critic_value_ = self.target_critic.forward(new_state, target_actions) 
+        q_expected = self.critic.forward(state, action)
+        q_targets = reward + self.discount_rate * critic_value_ * (1 - done)
 
-        Critic_loss = nn.MSELoss()(q_expected, q_targets.detach())
-        self.value_estimator.train()
-        self.value_estimator.optimizer.zero_grad()
-        Critic_loss.backward()
-        self.value_estimator.optimizer.step()
+        critic_loss = nn.MSELoss()(q_expected, q_targets.detach())
+        self.critic.train()
+        self.critic.optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic.optimizer.step()
 
-        self.policy.eval()
-        self.value_estimator.eval()
+        self.actor.eval()
+        self.critic.eval()
 
-        mu = self.policy.forward(state)
-        Actor_loss = -self.value_estimator.forward(state, mu)
+        mu = self.actor.forward(state)
+        Actor_loss = -self.critic.forward(state, mu)
 
         Actor_loss = T.mean(Actor_loss)
-        self.policy.train()
-        self.policy.optimizer.zero_grad()
+        self.actor.train()
+        self.actor.optimizer.zero_grad()
         Actor_loss.backward()
-        self.policy.optimizer.step()
+        self.actor.optimizer.step()
 
         self.update_network_parameters()
 
@@ -143,18 +149,18 @@ class DDPGAgent(LearningAgent):
         if SoftUpdateRate is None:
             SoftUpdateRate = self.soft_update_rate
 
-        Critic_state_dict = dict(self.value_estimator.named_parameters())
+        Critic_state_dict = dict(self.critic.named_parameters())
         Actor_state_dict = dict(self.policy.named_parameters())
-        TargetCritic_dict = dict(self.target_value_estimator.named_parameters())
-        TargetActor_dict = dict(self.target_policy.named_parameters())
+        TargetCritic_dict = dict(self.target_critic.named_parameters())
+        TargetActor_dict = dict(self.target_actor.named_parameters())
 
         for name in Critic_state_dict:
             Critic_state_dict[name] = SoftUpdateRate*Critic_state_dict[name].clone() + (1-SoftUpdateRate)*TargetCritic_dict[name].clone()
-        self.target_value_estimator.load_state_dict(Critic_state_dict)
+        self.target_critic.load_state_dict(Critic_state_dict)
 
         for name in Actor_state_dict:
             Actor_state_dict[name] = SoftUpdateRate*Actor_state_dict[name].clone() + (1-SoftUpdateRate)*TargetActor_dict[name].clone()
-        self.target_policy.load_state_dict(Actor_state_dict)
+        self.target_actor.load_state_dict(Actor_state_dict)
 
         """
         #Verify that the copy assignment worked correctly
@@ -173,10 +179,10 @@ class DDPGAgent(LearningAgent):
         """
     
     def save_models(self):
-        self.policy.save_checkpoint()
-        self.target_policy.save_checkpoint()
-        self.value_estimator.save_checkpoint()
-        self.target_value_estimator.save_checkpoint()
+        self.actor.save_checkpoint()
+        self.target_actor.save_checkpoint()
+        self.critic.save_checkpoint()
+        self.target_critic.save_checkpoint()
 
     def load_models(self):
         self.policy.load_checkpoint()
@@ -194,7 +200,7 @@ def DDPGAlgorithm(environment: MDPEnvironment, agent: DDPGAgent, n_episodes: int
         for _ in range(episode_duration):
             observation= agent.observe()
             action = agent.act(observation) 
-            new_state, reward, terminal_signal= environment.transition_step(environment.current_state, np.array(action), agent.ControlInterval) 
+            new_state, reward, terminal_signal= environment.transition_step(environment.current_state, np.array(action), agent.control_interval) 
             agent.memory.append((observation, action, agent.observe(new_state), reward, int(terminal_signal)))
             agent.learn()
             episode_return += reward
